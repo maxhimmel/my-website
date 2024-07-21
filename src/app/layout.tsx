b6a4e9a7d98c598722a142a5d59d39fe46d0a9ff
Max Himmel <himmelmax@gmail.com>
2024-07-21 15:24:22 -0700

    add navbar w/theme switch

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import "./globals.css";
+import { ThemeController } from "./components/themeController";
 
 export const metadata: Metadata = {
   title: "Max Himmel",
@@ -12,7 +13,18 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        <div className="sticky top-0 navbar shadow-xl z-50 bg-neutral text-neutral-content">
+          <div className="navbar-start">
+            <ThemeController />
+          </div>
+          <div className="navbar-center">
+            <p className="text-4xl font-black">Max Himmel</p>
+          </div>
+          <div className="navbar-end"></div>
+        </div>
+        {children}
+      </body>
     </html>
   );
 }
