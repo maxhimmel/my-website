9bd9acbe9edfc57163e1531096b4eceed38b1198
Max Himmel <himmelmax@gmail.com>
2024-07-25 16:24:09 -0700

    move page content into new page container

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -1,42 +1,14 @@
-import { ScrollVerticalAnimation } from "../svg/patterns/animations";
-import { HideoutPattern } from "../svg/patterns/hideout";
-import { ParkayFloorPattern } from "../svg/patterns/parkayFloor";
+import { PageContainer } from "../components/pageContainer";
 import { Timeline } from "./timeline";
 
 export default function Projects() {
   return (
-    <main>
-      <HideoutPattern
-        className="bg-base-100 text-accent shadow-2xl w-60 h-screen fixed left-0 z-10"
-        animations={[
-          <ScrollVerticalAnimation
-            key={1}
-            dur="2s"
-            repeatCount="indefinite"
-            direction="down"
-          />,
-        ]}
-      />
-      <ParkayFloorPattern className="bg-base-100 text-base-200 text-opacity-65 fixed h-screen w-screen" />
-      <HideoutPattern
-        className="xl:block hidden bg-base-100 text-accent shadow-2xl w-60 h-screen fixed right-0 z-10"
-        animations={[
-          <ScrollVerticalAnimation
-            key={1}
-            dur="2s"
-            repeatCount="indefinite"
-            direction="up"
-          />,
-        ]}
-      />
-
-      <div className="absolute xl:left-60 xl:right-60 left-60 overflow-y-auto">
-        <div className="hero">
-          <div className="hero-content flex flex-col">
-            <Timeline />
-          </div>
+    <PageContainer>
+      <div className="hero">
+        <div className="hero-content flex flex-col">
+          <Timeline />
         </div>
       </div>
-    </main>
+    </PageContainer>
   );
 }
