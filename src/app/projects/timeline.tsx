0552743bac13e4aab1184849f6f9d46287360804
Max Himmel <himmelmax@gmail.com>
2024-07-25 11:32:12 -0700

    simply entry usage further

--- src/app/projects/timeline.tsx
+++ src/app/projects/timeline.tsx
@@ -4,13 +4,11 @@ import { Tobuscus } from "./publications/toboscus";
 import { DeerHunter } from "./publications/deerHunter";
 
 export function Timeline() {
-  const entries = [<MarbleKnights />, <Tobuscus />, <DeerHunter />];
-
   return (
     <ul className="timeline timeline-snap-icon max-xl:timeline-compact timeline-vertical">
-      {entries.map((entry, i) => (
-        <React.Fragment key={i}>{entry}</React.Fragment>
-      ))}
+      <MarbleKnights />
+      <Tobuscus />
+      <DeerHunter />
     </ul>
   );
 }
