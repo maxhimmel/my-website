d98cc8fb5ebbb663466b48890c2c6564ef2fe0c5
Max Himmel <himmelmax@gmail.com>
2024-07-25 16:41:28 -0700

    create temp home screen

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,21 @@
+import Link from "next/link";
+import { PageContainer } from "./components/pageContainer";
+
 export default function Home() {
   return (
-    <div>
-      <h1>Home</h1>
-    </div>
+    <PageContainer>
+      <div className="hero">
+        <div className="hero-content">
+          <div className="menu menu-lg menu-vertical bg-base-200 rounded-box w-56">
+            <li>
+              <Link href="/projects">Projects</Link>
+            </li>
+            <li>
+              <Link href="/resume">Resume</Link>
+            </li>
+          </div>
+        </div>
+      </div>
+    </PageContainer>
   );
 }
