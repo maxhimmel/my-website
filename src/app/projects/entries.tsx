4396d858d0e73ed59d88443e00ae299461cf54fb
Max Himmel <himmelmax@gmail.com>
2024-07-25 09:46:33 -0700

    create entry element for project history

---
+++ src/app/projects/entries.tsx
@@ -0,0 +1,127 @@
+import { Entry } from "./entry";
+import { CarouselButton } from "../components/carouselButton";
+
+const videoWidth = 560;
+const videoHeight = 315;
+
+export const entries = [
+  <Entry
+    date="Unity"
+    title="Marbe Knights"
+    description="Sword-swinging fantasy meets marble mania! Roll into battle with Orbin, Marabelle, and the other Knights of the Round to defend King Rolland and the Kingdom of Roundingham from the evil Lord Terroball! Armed with your blade, your wits, and a variety of orb-based powers, you’ll smite foes, solve puzzles, and embark on an epic fantasy journey with up to three other players!"
+  >
+    <div
+      className={`carousel w-[${videoWidth}px] h-[${videoHeight}px]
+          [&_iframe]:border-primary
+          [&_iframe]:border-8
+          [&_iframe]:rounded-2xl`}
+    >
+      <div
+        id="mk-1"
+        className="carousel-item relative w-full [&_.btn]:hover:opacity-100"
+      >
+        <iframe
+          width={videoWidth}
+          height={videoHeight}
+          src="https://www.youtube-nocookie.com/embed/Ztq1wiz-jzk?si=tbGL--gEaWbsB_Z5"
+          title="YouTube video player"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+          referrerPolicy="strict-origin-when-cross-origin"
+          allowFullScreen
+        ></iframe>
+        <CarouselButton target={3} className="btn btn-primary">
+          ❮
+        </CarouselButton>
+        <CarouselButton target={2} className="btn btn-primary">
+          ❯
+        </CarouselButton>
+      </div>
+      <div
+        id="mk-2"
+        className="carousel-item relative w-full [&_.btn]:hover:opacity-100"
+      >
+        <iframe
+          width={videoWidth}
+          height={videoHeight}
+          src="https://www.youtube-nocookie.com/embed/GjuriQkEYO0?si=jXAPtiK6ei4BhXIe"
+          title="YouTube video player"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+          referrerPolicy="strict-origin-when-cross-origin"
+          allowFullScreen
+        ></iframe>
+        <CarouselButton target={1} className="btn btn-primary">
+          ❮
+        </CarouselButton>
+        <CarouselButton target={3} className="btn btn-primary">
+          ❯
+        </CarouselButton>
+      </div>
+      <div
+        id="mk-3"
+        className="carousel-item relative w-full [&_.btn]:hover:opacity-100"
+      >
+        <iframe
+          width={videoWidth}
+          height={videoHeight}
+          src="https://www.youtube-nocookie.com/embed/qxmljbh6pqQ?si=17kGQ9HUjNHz-aHe"
+          title="YouTube video player"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+          referrerPolicy="strict-origin-when-cross-origin"
+          allowFullScreen
+        ></iframe>
+        <CarouselButton target={2} className="btn btn-primary">
+          ❮
+        </CarouselButton>
+        <CarouselButton target={1} className="btn btn-primary">
+          ❯
+        </CarouselButton>
+      </div>
+    </div>
+  </Entry>,
+  <Entry
+    date="Unity"
+    title="Toboscus Adventures: Wizards"
+    description="When the Wizard of Darkness awakened, it was up to one man to defeat him. When that man was accidentally killed by one idiot, Tobuscus (or “person of extremely low intelligence” – gently put), the responsibility of becoming an AWESOME WIZARD fell onto him… by default!"
+  >
+    <div
+      className="
+        [&_iframe]:border-primary
+        [&_iframe]:border-8
+        [&_iframe]:rounded-2xl
+      "
+    >
+      <iframe
+        width={videoWidth}
+        height={videoHeight}
+        src="https://www.youtube-nocookie.com/embed/ddeiYOCGUlY?si=FbmprN1nAugbwsct"
+        title="YouTube video player"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+        referrerPolicy="strict-origin-when-cross-origin"
+        allowFullScreen
+      ></iframe>
+    </div>
+  </Entry>,
+  <Entry
+    date="Unity"
+    title="Deer Hunter: Reloaded"
+    description="Deer Hunter: Reloaded is a first-person hunting-simulation game based off of the #1 hunting franchise in the World. Deer Hunter will provide an authentic hunting experience for players as they venture to some of the top-rated hunting regions in North America, from the plains of West Texas to the rugged terrain of Alaska."
+  >
+    <div
+      className="
+        [&_iframe]:border-primary
+        [&_iframe]:border-8
+        [&_iframe]:rounded-2xl
+      "
+    >
+      <iframe
+        width={videoWidth}
+        height={videoHeight}
+        src="https://www.youtube-nocookie.com/embed/DRWbPPBm7PE?si=qiDZlEVOPEuaxGsq"
+        title="YouTube video player"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+        referrerPolicy="strict-origin-when-cross-origin"
+        allowFullScreen
+      ></iframe>
+    </div>
+  </Entry>,
+];
---
+++ src/app/projects/entry.tsx
@@ -0,0 +1,54 @@
+import React from "react";
+
+export function Entry({
+  date,
+  title,
+  description,
+  children,
+}: {
+  date: string;
+  title: string;
+  description: React.ReactNode;
+  children?: React.ReactNode;
+}) {
+  return (
+    <li
+      className="
+        [&_.timeline-entry]:odd:timeline-end
+        [&_.timeline-entry]:odd:xl:timeline-start
+        [&_.timeline-entry]:even:timeline-end
+        
+        [&_.timeline-entry]:text-start
+        [&_.timeline-entry]:odd:xl:text-end
+        
+        [&_.child-content]:odd:justify-start
+        [&_.child-content]:odd:xl:justify-end
+        [&_.child-content]:even:justify-start
+        
+        [&_.hr-t]:first:hidden
+        [&_.hr-b]:last:hidden
+        "
+    >
+      <hr className="hr-t" />
+      <Icon />
+      <div className="timeline-entry bg-base-200 shadow-lg rounded-lg p-4 m-4">
+        <time className="font-mono italic">{date}</time>
+        <div className="text-lg font-black">{title}</div>
+        <p className="drop-shadow-sm">{description}</p>
+        <div className="child-content grid mt-2">{children}</div>
+        <div className="mb-14" />
+      </div>
+      <hr className="hr-b" />
+    </li>
+  );
+}
+
+function Icon() {
+  return (
+    <div className="timeline-middle">
+      <div className="size-5 p-0.5">
+        <div className="size-full bg-current rounded-full" />
+      </div>
+    </div>
+  );
+}
