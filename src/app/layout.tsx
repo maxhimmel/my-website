75c190c320b6e211f7b8c3ae5d395aa62c225aa3
Max Himmel <himmelmax@gmail.com>
2024-07-25 09:40:48 -0700

    include support for tailwind breakpoints

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,14 @@
-import type { Metadata } from "next";
+import type { Metadata, Viewport } from "next";
 import "./globals.css";
 import { ThemeController } from "./components/themeController";
 
 export const metadata: Metadata = {
   title: "Max Himmel",
+};
+
+export const viewport: Viewport = {
+  width: "device-width",
+  initialScale: 1,
 };
 
 export default function RootLayout({
