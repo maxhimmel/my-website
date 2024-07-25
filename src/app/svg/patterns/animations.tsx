e3c362ddc6168c1e9ca54a47afc8bbd86ec6fc69
Max Himmel <himmelmax@gmail.com>
2024-07-25 09:43:26 -0700

    support SVG animations
    + include two premade anims (vert/hori scrolling)

---
+++ src/app/svg/patterns/animations.tsx
@@ -0,0 +1,58 @@
+"use client";
+
+import { createContext, useContext } from "react";
+
+/* --- */
+
+const patternSizeContext = createContext({
+  width: 0,
+  height: 0,
+});
+export const PatternSizeProvider = patternSizeContext.Provider;
+export const usePatternSize = () => useContext(patternSizeContext);
+
+/* --- */
+
+export function ScrollVerticalAnimation({
+  dur,
+  direction,
+  repeatCount,
+}: {
+  dur: string;
+  direction: "up" | "down";
+  repeatCount: string;
+}) {
+  const { height } = usePatternSize();
+  const sign = direction === "up" ? "-" : "";
+
+  return (
+    <animate
+      attributeName="y"
+      values={`0px; ${sign}${height}px`}
+      dur={dur}
+      repeatCount={repeatCount}
+    />
+  );
+}
+
+export function ScrollHorizontalAnimation({
+  dur,
+  direction,
+  repeatCount,
+}: {
+  dur: string;
+  direction: "left" | "right";
+  repeatCount: string;
+}) {
+  const { width } = usePatternSize();
+  const sign = direction === "left" ? "-" : "";
+
+  return (
+    <animate
+      attributeName="x"
+      values={`${sign}${width}px; 0px`}
+      dur={dur}
+      repeatCount={repeatCount}
+    />
+  );
+}
--- src/app/svg/patterns/pattern.tsx
+++ src/app/svg/patterns/pattern.tsx
@@ -1,6 +1,8 @@
-import React from "react";
+"use client";
 
-let id: number = 0;
+import React from "react";
+import { PatternSizeProvider } from "./animations";
+import { randomBytes } from "crypto";
 
 export function Pattern({
   className,
@@ -8,37 +10,20 @@ export function Pattern({
   shapes,
   viewBoxWidth,
   viewBoxHeight,
+  animations,
 }: {
   className?: string;
   uuid?: string;
   shapes: React.ReactNode[];
   viewBoxWidth: number;
   viewBoxHeight: number;
+  animations?: React.ReactNode[];
 }) {
   if (!uuid) {
-    uuid = `${Pattern.name}_${id++}`;
+    uuid = `${Pattern.name}_${randomBytes(16).toString("hex")}`;
   }
 
   return (
-    // <div
-    //   className={`${className} fill-current`}
-    //   style={{
-    //     backgroundImage: `url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='52' height='52' viewBox='0 0 52 52'%3E%3Cpath fill='%239C92AC' fill-opacity='0.4' d='M0 17.83V0h17.83a3 3 0 0 1-5.66 2H5.9A5 5 0 0 1 2 5.9v6.27a3 3 0 0 1-2 5.66zm0 18.34a3 3 0 0 1 2 5.66v6.27A5 5 0 0 1 5.9 52h6.27a3 3 0 0 1 5.66 0H0V36.17zM36.17 52a3 3 0 0 1 5.66 0h6.27a5 5 0 0 1 3.9-3.9v-6.27a3 3 0 0 1 0-5.66V52H36.17zM0 31.93v-9.78a5 5 0 0 1 3.8.72l4.43-4.43a3 3 0 1 1 1.42 1.41L5.2 24.28a5 5 0 0 1 0 5.52l4.44 4.43a3 3 0 1 1-1.42 1.42L3.8 31.2a5 5 0 0 1-3.8.72zm52-14.1a3 3 0 0 1 0-5.66V5.9A5 5 0 0 1 48.1 2h-6.27a3 3 0 0 1-5.66-2H52v17.83zm0 14.1a4.97 4.97 0 0 1-1.72-.72l-4.43 4.44a3 3 0 1 1-1.41-1.42l4.43-4.43a5 5 0 0 1 0-5.52l-4.43-4.43a3 3 0 1 1 1.41-1.41l4.43 4.43c.53-.35 1.12-.6 1.72-.72v9.78zM22.15 0h9.78a5 5 0 0 1-.72 3.8l4.44 4.43a3 3 0 1 1-1.42 1.42L29.8 5.2a5 5 0 0 1-5.52 0l-4.43 4.44a3 3 0 1 1-1.41-1.42l4.43-4.43a5 5 0 0 1-.72-3.8zm0 52c.13-.6.37-1.19.72-1.72l-4.43-4.43a3 3 0 1 1 1.41-1.41l4.43 4.43a5 5 0 0 1 5.52 0l4.43-4.43a3 3 0 1 1 1.42 1.41l-4.44 4.43c.36.53.6 1.12.72 1.72h-9.78zm9.75-24a5 5 0 0 1-3.9 3.9v6.27a3 3 0 1 1-2 0V31.9a5 5 0 0 1-3.9-3.9h-6.27a3 3 0 1 1 0-2h6.27a5 5 0 0 1 3.9-3.9v-6.27a3 3 0 1 1 2 0v6.27a5 5 0 0 1 3.9 3.9h6.27a3 3 0 1 1 0 2H31.9z'%3E%3C/path%3E%3C/svg%3E")`,
-    //   }}
-
-    //   // we're working!
-    //   // className={className}
-    //   // style={{
-    //   //   backgroundImage: `url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='4' height='4' viewBox='0 0 4 4'%3E%3Cpath fill-opacity='0.4' d='M1 3h1v1H1V3zm2-2h1v1H3V1z'%3E%3C/path%3E%3C/svg%3E")`,
-    //   // }}
-
-    //   // pure ass below:
-    //   // this actually works at least but the patterns are totally off on size
-    //   // className={`${className} bg-[url(/formal-invitation.svg)]`}
-    //   // this doesn't even work
-    //   // className={`${className} bg-[url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='4' height='4' viewBox='0 0 4 4'%3E%3Cpath fill='%239C92AC' fill-opacity='0.4' d='M1 3h1v1H1V3zm2-2h1v1H3V1z'%3E%3C/path%3E%3C/svg%3E")]`}
-    // ></div>
-
     <svg
       className={`fill-current ${className}`}
       xmlns="http://www.w3.org/2000/svg"
@@ -53,9 +38,19 @@ export function Pattern({
           {shapes.map((shape, i) => (
             <React.Fragment key={i}>{shape}</React.Fragment>
           ))}
+
+          {animations && (
+            <PatternSizeProvider
+              value={{ width: viewBoxWidth, height: viewBoxHeight }}
+            >
+              {animations.map((anim, i) => (
+                <React.Fragment key={i}>{anim}</React.Fragment>
+              ))}
+            </PatternSizeProvider>
+          )}
         </pattern>
       </defs>
-      <rect width="100%" height="100%" fill={`url(#${uuid})`} />
+      <rect width="100%" height="100%" fill={`url(#${uuid})`}></rect>
     </svg>
   );
 }
