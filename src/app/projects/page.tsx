36365ce1a95dec560b583542e65eb113b7aa074a
Max Himmel <himmelmax@gmail.com>
2024-07-25 09:47:08 -0700

    create project history page

---
+++ src/app/projects/page.tsx
@@ -0,0 +1,40 @@
+import { ScrollVerticalAnimation } from "../svg/patterns/animations";
+import { HideoutPattern } from "../svg/patterns/hideout";
+import { ParkayFloorPattern } from "../svg/patterns/parkayFloor";
+import { Timeline } from "./timeline";
+
+export default function Projects() {
+  return (
+    <main>
+      <HideoutPattern
+        className="bg-base-100 text-accent shadow-2xl w-60 h-screen fixed left-0 z-10"
+        animations={[
+          <ScrollVerticalAnimation
+            dur="2s"
+            repeatCount="indefinite"
+            direction="down"
+          />,
+        ]}
+      />
+      <ParkayFloorPattern className="bg-base-100 text-base-200 text-opacity-65 fixed h-screen w-screen" />
+      <HideoutPattern
+        className="xl:block hidden bg-base-100 text-accent shadow-2xl w-60 h-screen fixed right-0 z-10"
+        animations={[
+          <ScrollVerticalAnimation
+            dur="2s"
+            repeatCount="indefinite"
+            direction="up"
+          />,
+        ]}
+      />
+
+      <div className="absolute xl:left-60 xl:right-60 left-60 overflow-y-auto">
+        <div className="hero">
+          <div className="hero-content flex flex-col">
+            <Timeline />
+          </div>
+        </div>
+      </div>
+    </main>
+  );
+}
