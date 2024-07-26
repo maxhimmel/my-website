adda8f1e7504324a87d8ac2efe4b59a80575e414
Max Himmel <himmelmax@gmail.com>
2024-07-25 17:48:24 -0700

    fix media breakpoint when mobile is landscape

--- src/app/components/pageContainer.tsx
+++ src/app/components/pageContainer.tsx
@@ -2,7 +2,7 @@ export function PageContainer({ children }: { children: React.ReactNode }) {
   return (
     <main id="page-container">
       <div
-        className={`absolute xl:left-60 xl:right-60 left-60 overflow-y-auto top-16 bottom-0`}
+        className={`absolute overflow-y-auto xl:right-60 left-60 right-0 top-16 bottom-0`}
       >
         {children}
       </div>
