5ecb946f634c5ae54316fcbe1ec33416ae19822f
Max Himmel <himmelmax@gmail.com>
2024-07-25 18:45:30 -0700

    wrap bg pillars in divs for more intuitive nesting of navmenu

--- src/app/components/backgroundLayout.tsx
+++ src/app/components/backgroundLayout.tsx
@@ -6,32 +6,38 @@ import { NavMenu } from "./navMenu";
 export function BackgroundLayout() {
   return (
     <div id="bg-layout">
-      <HideoutPattern
-        className="bg-base-100 text-accent shadow-2xl w-60 h-screen fixed left-0 z-10"
-        animations={[
-          <ScrollVerticalAnimation
-            key={1}
-            dur="2s"
-            repeatCount="indefinite"
-            direction="down"
-          />,
-        ]}
-      />
-      <div className="absolute top-20 left-4 w-52 z-10">
-        <NavMenu />
+      <div className="shadow-2xl w-60 pt-16 h-screen fixed left-0 z-10">
+        <HideoutPattern
+          className="absolute bg-base-100 text-accent size-full"
+          animations={[
+            <ScrollVerticalAnimation
+              key={1}
+              dur="2s"
+              repeatCount="indefinite"
+              direction="down"
+            />,
+          ]}
+        />
+        <div className="relative m-4">
+          <NavMenu />
+        </div>
       </div>
+
       <ParkayFloorPattern className="bg-base-100 text-base-200 text-opacity-65 fixed h-screen w-screen" />
-      <HideoutPattern
-        className="xl:block hidden bg-base-100 text-accent shadow-2xl w-60 h-screen fixed right-0 z-10"
-        animations={[
-          <ScrollVerticalAnimation
-            key={1}
-            dur="2s"
-            repeatCount="indefinite"
-            direction="up"
-          />,
-        ]}
-      />
+
+      <div className="xl:block hidden shadow-2xl w-60 pt-16 h-screen fixed right-0 z-10">
+        <HideoutPattern
+          className="absolute bg-base-100 text-accent size-full"
+          animations={[
+            <ScrollVerticalAnimation
+              key={1}
+              dur="2s"
+              repeatCount="indefinite"
+              direction="up"
+            />,
+          ]}
+        />
+      </div>
     </div>
   );
 }
