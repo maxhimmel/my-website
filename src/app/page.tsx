cbe4999c6a06c147ddf0974b8b2e753aefacf086
Max Himmel <himmelmax@gmail.com>
2024-07-25 17:41:37 -0700

    move main page navigation into navMenu component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,12 @@
-import Link from "next/link";
 import { PageContainer } from "./components/pageContainer";
+import { NavMenu } from "./components/navMenu";
 
 export default function Home() {
   return (
     <PageContainer>
       <div className="hero">
         <div className="hero-content">
-          <div className="menu menu-lg menu-vertical bg-base-200 rounded-box w-56">
-            <li>
-              <Link href="/projects">Projects</Link>
-            </li>
-            <li>
-              <Link href="/resume">Resume</Link>
-            </li>
-          </div>
+          <NavMenu />
         </div>
       </div>
     </PageContainer>
