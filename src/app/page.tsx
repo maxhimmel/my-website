4b0fb5232515127daf272748a510fe5ab272abae
Max Himmel <himmelmax@gmail.com>
2024-07-25 18:28:34 -0700

    redirect main page to projects page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,10 @@
 import { PageContainer } from "./components/pageContainer";
 import { NavMenu } from "./components/navMenu";
+import { redirect } from "next/navigation";
 
 export default function Home() {
+  redirect("/projects");
+
   return (
     <PageContainer>
       <div className="hero">
