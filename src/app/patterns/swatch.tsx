f87d672548d070887c4d79d1a8cd91a2b351080a
Max Himmel <himmelmax@gmail.com>
2024-07-26 17:42:07 -0700

    add hover and animation state to pattern swatches

--- src/app/patterns/swatch.tsx
+++ src/app/patterns/swatch.tsx
@@ -12,7 +12,13 @@ export function Swatch({
   const patternName = PatternElement.name.replace("Pattern", "");
 
   return (
-    <div className="border-2 border-base-300 rounded-lg h-60 relative">
+    <div
+      className="
+        border-2 border-base-300 rounded-lg h-60 relative 
+        hover:scale-105 hover:shadow-2xl hover:z-10
+        transition duration-[0.15s]
+      "
+    >
       <PatternElement isBackground className={patternClassName}>
         <div className="absolute bottom-0 py-2 px-4 bg-base-300 w-full">
           <h1 className="font-semibold text-xl drop-shadow-md">
