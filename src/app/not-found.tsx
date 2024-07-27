b07c0cdd00773f1b2e9b9090b13b9bbb6e1e7bc2
Max Himmel <himmelmax@gmail.com>
2024-07-26 17:04:21 -0700

    attempt 2 to fix text color not being built correctly

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -3,7 +3,7 @@ import { PageContainer } from "./components/pageContainer";
 export default function PageNotFound() {
   return (
     <PageContainer>
-      <div className="flex flex-col flex-1 items-center justify-center text-current">
+      <div className="flex flex-col flex-1 items-center justify-center text-base-content">
         <h1 className="text-9xl font-bold">404</h1>
         <h2 className="text-4xl font-semibold">Page Not Found</h2>
       </div>
