1aa3af961c6fd6da4011b5d8f1e0d6710f5aecf0
Max Himmel <himmelmax@gmail.com>
2024-07-28 15:57:02 -0700

    side pillars have min width
    + this prevents them from being squished on mobile

--- src/app/components/pageContainer.tsx
+++ src/app/components/pageContainer.tsx
@@ -2,9 +2,9 @@ export function PageContainer({ children }: { children: React.ReactNode }) {
   return (
     <main id="page-container">
       <div className="flex flex-row relative">
-        <div className="w-60 h-screen" />
+        <div className="min-w-60 w-60 h-screen" />
         <div className="pt-16 flex flex-1">{children}</div>
-        <div className="hidden xl:block w-60 h-screen" />
+        <div className="hidden xl:block min-w-60 w-60 h-screen" />
       </div>
     </main>
   );
