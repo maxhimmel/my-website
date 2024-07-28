468a6dc58e9d964256c98d0ee711a5789c71726d
Max Himmel <himmelmax@gmail.com>
2024-07-28 15:56:34 -0700

    add reverse spin animation

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -11,6 +11,21 @@ const config: Config = {
 
   daisyui: {
     themes: ["valentine", "dim"]
+  },
+
+  theme: {
+    extend: {
+      keyframes: {
+        "spin-reverse": {
+          "0%": {
+            transform: "rotate(0deg)",
+          },
+          "100%": {
+            transform: "rotate(-360deg)",
+          },
+        }
+      },
+    },
   }
 };
 export default config;
