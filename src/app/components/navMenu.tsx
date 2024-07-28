3149f8474472c146443b2ffb1b3ca7f5e06cc860
Max Himmel <himmelmax@gmail.com>
2024-07-28 15:57:46 -0700

    add about me button to nav menu

--- src/app/components/navMenu.tsx
+++ src/app/components/navMenu.tsx
@@ -3,7 +3,7 @@
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 
-export function NavMenu() {
+export function NavMenu({ className }: { className?: string }) {
   const pathname = usePathname();
 
   function getActiveClass(path: string) {
@@ -14,11 +14,24 @@ export function NavMenu() {
 
   return (
     <div
-      className="menu menu-lg bg-base-200 shadow-xl text-base-content rounded-box
+      className={`${className} menu menu-lg bg-base-200 shadow-xl text-base-content rounded-box
         border-4 border-accent
         [&_a]:rounded-2xl
-        "
+        `}
     >
+      <li>
+        <Link className={getActiveClass("/about-me")} href="/about-me">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 24 24"
+            fill="currentColor"
+            className="size-6"
+          >
+            <path d="m11.645 20.91-.007-.003-.022-.012a15.247 15.247 0 0 1-.383-.218 25.18 25.18 0 0 1-4.244-3.17C4.688 15.36 2.25 12.174 2.25 8.25 2.25 5.322 4.714 3 7.688 3A5.5 5.5 0 0 1 12 5.052 5.5 5.5 0 0 1 16.313 3c2.973 0 5.437 2.322 5.437 5.25 0 3.925-2.438 7.111-4.739 9.256a25.175 25.175 0 0 1-4.244 3.17 15.247 15.247 0 0 1-.383.219l-.022.012-.007.004-.003.001a.752.752 0 0 1-.704 0l-.003-.001Z" />
+          </svg>
+          About Me
+        </Link>
+      </li>
       <li>
         <Link className={getActiveClass("/projects")} href="/projects">
           <svg
