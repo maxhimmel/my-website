2682fae06daf6d4caacd3f1000273a492768ba13
Max Himmel <himmelmax@gmail.com>
2024-07-29 09:37:56 -0700

    move navbar from layout into bespoke component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { BackgroundLayout } from "./components/backgroundLayout";
 import { ThemeDropdown } from "./components/themeDropdown";
 import "./globals.css";
 import { PolkaDotsPattern } from "./svg/patterns/polkaDots";
+import { Navbar } from "./components/navbar";
 
 export const metadata: Metadata = {
   title: "Max Himmel",
@@ -21,19 +22,8 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body>
-        <div className="fixed top-0 navbar bg-neutral text-neutral-content shadow-xl z-50 border-b-4 border-neutral-content">
-          <PolkaDotsPattern className="bg-neutral text-neutral-content opacity-10 absolute inset-0 size-full -z-10" />
-          <div className="navbar-start">
-            <ThemeDropdown />
-          </div>
-          <div className="navbar-center">
-            <p className="text-4xl font-black">Max Himmel</p>
-          </div>
-          <div className="navbar-end"></div>
-        </div>
-
+        <Navbar />
         <BackgroundLayout />
-
         {children}
       </body>
     </html>
