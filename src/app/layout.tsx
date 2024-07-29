58965a930dfc2d3ea1eaac8e2ad48efb77d0b482
Max Himmel <himmelmax@gmail.com>
2024-07-29 09:32:57 -0700

    use theme dropdown instead of toggle

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata, Viewport } from "next";
 import { BackgroundLayout } from "./components/backgroundLayout";
-import { ThemeController } from "./components/themeController";
+import { ThemeDropdown } from "./components/themeDropdown";
 import "./globals.css";
 import { PolkaDotsPattern } from "./svg/patterns/polkaDots";
 
@@ -24,7 +24,7 @@ export default function RootLayout({
         <div className="fixed top-0 navbar bg-neutral text-neutral-content shadow-xl z-50 border-b-4 border-neutral-content">
           <PolkaDotsPattern className="bg-neutral text-neutral-content opacity-10 absolute inset-0 size-full -z-10" />
           <div className="navbar-start">
-            <ThemeController />
+            <ThemeDropdown />
           </div>
           <div className="navbar-center">
             <p className="text-4xl font-black">Max Himmel</p>
