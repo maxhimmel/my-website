563e186c7a70884c819886f5c8a26b93df748fe6
Max Himmel <himmelmax@gmail.com>
2025-03-24 15:41:04 -0700

    hook up all sections into home page
    + with scrolling hero banner!

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,21 @@
+import { AboutMe } from "./about/about";
+import { ScrollVerticalAnimation } from "./patterns/svg/animations";
+import { TicTacToePattern } from "./patterns/svg/ticTacToe";
+import { Portfolio } from "./portfolio/portfolio";
+import { SkillsBanner } from "./skills/skills";
+
 export default function Home() {
   return (
     <>
-      <div className="h-dvh grow"></div>
+      <div className="w-full h-72">
+        <TicTacToePattern
+          className="bg-base-300 text-primary size-full"
+          animations={[<ScrollVerticalAnimation key={1} direction="up" dur="3s" repeatCount="indefinite" />]}
+        />
+      </div>
+      <SkillsBanner />
+      <Portfolio />
+      <AboutMe />
     </>
   );
 }
