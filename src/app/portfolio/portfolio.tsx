28905ebb990e703b119a96f15298e309e27dea62
Max Himmel <himmelmax@gmail.com>
2025-03-25 11:28:15 -0700

    import and use portfolio data

--- src/app/portfolio/portfolio.tsx
+++ src/app/portfolio/portfolio.tsx
@@ -1,6 +1,8 @@
-import { RiExternalLinkFill } from "react-icons/ri";
-import { AnchorScrollOffset } from "../lib/anchorScrollOffset";
+import Image from "next/image";
+import { RiExternalLinkFill, RiLockFill } from "react-icons/ri";
 import { SiGithub } from "react-icons/si";
+import { Project, PROJECTS } from "../data/portfolio";
+import { AnchorScrollOffset } from "../lib/anchorScrollOffset";
 
 export function Portfolio() {
   return (
@@ -8,34 +10,50 @@ export function Portfolio() {
       <AnchorScrollOffset id="portfolio" />
       {/* <h2 className="text-4xl font-black">Work</h2> */}
       {/* <div className="divider"></div> */}
-      <div className="grid grid-cols-2 gap-4 justify-items-center">
-        <Work />
-        <Work />
-        <Work />
-        <Work />
+      <div className="grid grid-cols-[repeat(auto-fill,400px)] gap-4 justify-center justify-items-center">
+        {PROJECTS.map((p) => (
+          <Work key={p.name} project={p} />
+        ))}
       </div>
     </div>
   );
 }
 
-function Work() {
+function Work({ project }: { project: Project }) {
   return (
-    <div className="card bg-base-300 w-96 shadow-sm">
-      <figure>
-        <img src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp" alt="Shoes" />
+    <div className="card bg-base-300 w-96 shadow-sm border-4 border-neutral">
+      <figure className="h-[192px] bg-neutral">
+        <Image
+          src={project.img}
+          alt={project.name}
+          width={400}
+          height={300}
+          priority
+          className="bg-center bg-cover"
+        />
       </figure>
       <div className="card-body">
-        <h2 className="card-title">Scratch Notes</h2>
-        <p>A card component has a figure, a body part, and inside body there are title and actions parts</p>
+        <h2 className="card-title">{project.name}</h2>
+        <p>{project.desc}</p>
         <div className="card-actions justify-end">
-          <a className="btn btn-primary">
+          <a href={project.referenceLink} className="btn btn-accent" target="_blank">
             Launch <RiExternalLinkFill className="size-5/12" />
           </a>
-          <a className="btn btn-secondary">
-            Source <SiGithub className="size-5/12" />
-          </a>
+          <SourceButton sourceLink={project.sourceLink} />
         </div>
       </div>
     </div>
+  );
+}
+
+function SourceButton({ sourceLink }: { sourceLink?: string }) {
+  return sourceLink ? (
+    <a href={sourceLink} className="btn btn-secondary" target="_blank">
+      Source <SiGithub className="size-5/12" />
+    </a>
+  ) : (
+    <a className="btn btn-disabled">
+      Source <RiLockFill className="size-5/12" />
+    </a>
   );
 }
