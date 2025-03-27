a9e2212101c23e53c162a9c2d65b4bdabc83e7c1
Max Himmel <himmelmax@gmail.com>
2025-03-27 11:58:03 -0700

    add tooltip for tech stack

--- src/app/portfolio/portfolio.tsx
+++ src/app/portfolio/portfolio.tsx
@@ -35,15 +35,20 @@ function Work({ project }: { project: Project }) {
       <div className="card-body relative">
         <div className="flex justify-between">
           <h2 className="card-title">{project.name}</h2>
-          <div className="absolute bottom-0 right-0 flex items-center gap-1 bg-neutral rounded-xl rounded-e-none p-2 pb-3">
-            {project.techStack.map((tech) => (
-              <img
-                key={tech}
-                alt={tech}
-                className="size-5"
-                src={`https://cdn.simpleicons.org/${tech}/white`}
-              />
-            ))}
+          <div
+            className="tooltip absolute bottom-0 right-0"
+            data-tip={project.techStack.map((tech) => tech.toLocaleUpperCase()).join(" · ")}
+          >
+            <div className="flex items-center gap-1 bg-neutral rounded-xl rounded-e-none p-2 pb-3">
+              {project.techStack.map((tech) => (
+                <img
+                  key={tech}
+                  alt={tech}
+                  className="size-5"
+                  src={`https://cdn.simpleicons.org/${tech}/white`}
+                />
+              ))}
+            </div>
           </div>
         </div>
         <i className="mb-4">{project.desc}</i>
