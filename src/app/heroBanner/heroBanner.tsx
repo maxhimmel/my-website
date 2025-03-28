0a72b5547f691c0d70c0a721631bf95aed1370fe
Max Himmel <himmelmax@gmail.com>
2025-03-27 17:58:40 -0700

    change tictactoe to topography hero banner

--- src/app/heroBanner/heroBanner.tsx
+++ src/app/heroBanner/heroBanner.tsx
@@ -1,21 +1,28 @@
-import { ScrollVerticalAnimation } from "../patterns/svg/animations";
+import { ScrollHorizontalAnimation, ScrollVerticalAnimation } from "../patterns/svg/animations";
 import { TicTacToePattern } from "../patterns/svg/ticTacToe";
+import { TopographyPattern } from "../patterns/svg/topography";
 
 const ANIMATION = {
   direction: "up" as const,
-  dur: "6s",
+  dur: "60s",
   repeatCount: "indefinite",
 };
 
 export function HeroBanner() {
   return (
-    <div className="w-full h-56">
-      <TicTacToePattern
+    <div className="w-full h-56 border-t-2 border-b-2 border-primary shadow-xl shadow-neutral">
+      <TopographyPattern
         className="bg-base-300 text-primary size-full"
         animations={[
           <ScrollVerticalAnimation
             key={1}
             direction={ANIMATION.direction}
+            dur={ANIMATION.dur}
+            repeatCount={ANIMATION.repeatCount}
+          />,
+          <ScrollHorizontalAnimation
+            key={2}
+            direction={"left"}
             dur={ANIMATION.dur}
             repeatCount={ANIMATION.repeatCount}
           />,
