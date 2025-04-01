34d066a1cd005f3ab187bac2b8d60048b11ca22d
maxhimmel <himmelmax@gmail.com>
2025-04-01 11:37:56 -0700

    only render extra dark shadows on dark mode

--- src/app/navbar/navbar.tsx
+++ src/app/navbar/navbar.tsx
@@ -5,7 +5,7 @@ import { ThemeToggle } from "../theme/themeToggle";
 
 export function Navbar() {
   return (
-    <div className="navbar bg-base-300 sticky top-0 z-10 border-b-8 border-primary shadow-xl shadow-neutral">
+    <div className="navbar bg-base-300 sticky top-0 z-10 border-b-8 border-primary shadow-xl dark:shadow-neutral">
       <div className="navbar-start">
         <div className="dropdown">
           <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
@@ -13,7 +13,7 @@ export function Navbar() {
           </div>
           <ul
             tabIndex={0}
-            className="menu menu-md dropdown-content absolute top-full mt-4 -left-2 w-dvw bg-transparent backdrop-blur-3xl rounded-box rounded-t-none z-1 p-2 shadow-lg shadow-neutral border-2 border-primary"
+            className="menu menu-md dropdown-content absolute top-full mt-4 -left-2 w-dvw bg-transparent backdrop-blur-3xl rounded-box rounded-t-none z-1 p-2 shadow-lg dark:shadow-neutral border-2 border-primary"
           >
             <NavLinks />
           </ul>
--- src/app/skills/skills.tsx
+++ src/app/skills/skills.tsx
@@ -19,7 +19,7 @@ import { AnchorScrollOffset } from "../lib/anchorScrollOffset";
 
 export function SkillsBanner() {
   return (
-    <div className="flex w-full bg-base-200 overflow-x-clip border-t-2 border-primary shadow-xl shadow-neutral">
+    <div className="flex w-full bg-base-200 overflow-x-clip border-t-2 border-primary shadow-xl dark:shadow-neutral">
       <AnchorScrollOffset id="skills" />
       <Skills />
       <Skills />
