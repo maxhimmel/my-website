597423d1653769e09de9a7e3cc63552bf77b4b34
Max Himmel <himmelmax@gmail.com>
2025-04-08 14:22:03 -0700

    wrap next config export with payload

--- next.config.mjs
+++ next.config.mjs
@@ -1,3 +1,5 @@
+import withPayload from "@payloadcms/next/withPayload";
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
@@ -14,4 +16,4 @@ const nextConfig = {
   },
 };
 
-export default nextConfig;
+export default withPayload(nextConfig);
