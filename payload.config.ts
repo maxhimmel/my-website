15ff27d7ca646d878b86da669d1bfd426c58b186
Max Himmel <himmelmax@gmail.com>
2025-04-10 10:28:13 -0700

    fix querying picture error?!

--- payload.config.ts
+++ payload.config.ts
@@ -9,6 +9,8 @@ import { Media } from "./src/models/media";
 import { vercelBlobStorage } from "@payloadcms/storage-vercel-blob";
 
 export default buildConfig({
+  csrf: ["https://maxhimmel.me", "http://localhost:3000"],
+
   // If you'd like to use Rich Text, pass your editor here
   editor: lexicalEditor(),
 
