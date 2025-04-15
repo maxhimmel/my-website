1e99a5c43ef5544686d73636c42deaa6c4a0d6fd
Max Himmel <himmelmax@gmail.com>
2025-04-15 10:00:56 -0700

    import cool velocity scroll example
    + https://motion.dev/docs/react-scroll-animations#scroll-velocity-and-direction

---
+++ src/app/(my-website)/skills/parallaxText.tsx
@@ -0,0 +1,60 @@
+"use client";
+
+import {
+  motion,
+  useAnimationFrame,
+  useMotionValue,
+  useScroll,
+  useSpring,
+  useTransform,
+  useVelocity,
+  wrap,
+} from "motion/react";
+import { PropsWithChildren, useRef } from "react";
+
+type ParallaxProps = PropsWithChildren & {
+  baseVelocity: number;
+};
+
+export function ParallaxText({ children, baseVelocity = 100 }: ParallaxProps) {
+  const baseX = useMotionValue(0);
+  const { scrollY } = useScroll();
+  const scrollVelocity = useVelocity(scrollY);
+  const smoothVelocity = useSpring(scrollVelocity, {
+    damping: 50,
+    stiffness: 400,
+  });
+  const velocityFactor = useTransform(smoothVelocity, [0, 1000], [0, 5], {
+    clamp: false,
+  });
+
+  const x = useTransform(baseX, (v) => `${wrap(-50, -0, v)}%`);
+
+  const directionFactor = useRef<number>(1);
+  useAnimationFrame((t, delta) => {
+    let moveBy = directionFactor.current * baseVelocity * (delta / 1000);
+
+    /**
+     * This is what changes the direction of the scroll once we
+     * switch scrolling directions.
+     */
+    if (velocityFactor.get() < 0) {
+      directionFactor.current = -1;
+    } else if (velocityFactor.get() > 0) {
+      directionFactor.current = 1;
+    }
+
+    moveBy += directionFactor.current * moveBy * velocityFactor.get();
+
+    baseX.set(baseX.get() + moveBy);
+  });
+
+  return (
+    <div className="flex overflow-hidden whitespace-nowrap flex-nowrap">
+      <motion.div className="flex whitespace-nowrap flex-nowrap" style={{ x }}>
+        {children}
+        {children}
+      </motion.div>
+    </div>
+  );
+}
