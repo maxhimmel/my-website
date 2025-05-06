8fdb298a20c31111804370cece37d9426472d6cf
Max Himmel <himmelmax@gmail.com>
2025-05-06 09:56:25 -0700

    WIP navbar mockup

--- src/app/(my-website)/navbar/navbar.tsx
+++ src/app/(my-website)/navbar/navbar.tsx
@@ -1,54 +1,58 @@
 import Link from "next/link";
-import { RiMenuFill } from "react-icons/ri";
-import { DropdownLink } from "../lib/dropdownLink";
-import { ThemeToggle } from "../theme/themeToggle";
+
+import {
+  NavigationMenu,
+  NavigationMenuItem,
+  NavigationMenuLink,
+  NavigationMenuList,
+} from "@/components/ui/navigation-menu";
 
 export function Navbar() {
-  return (
-    <div className="navbar bg-base-300 sticky top-0 z-10 border-b-8 border-primary shadow-xl dark:shadow-neutral">
-      <div className="navbar-start">
-        <div className="dropdown">
-          <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
-            <RiMenuFill className="size-6" />
-          </div>
-          <ul
-            tabIndex={0}
-            className="menu menu-md dropdown-content absolute top-full mt-4 -left-2 w-dvw bg-transparent backdrop-blur-3xl rounded-box rounded-t-none z-1 p-2 shadow-lg dark:shadow-neutral border-2 border-primary"
-          >
-            <NavLinks />
-          </ul>
-        </div>
-        <Link href="/" className="btn btn-ghost font-black text-3xl">
-          Max Himmel
-        </Link>
-      </div>
-      <div className="navbar-end">
-        <ul className="menu menu-horizontal px-1 hidden lg:flex">
-          <NavLinks />
-        </ul>
-        <ThemeToggle />
-      </div>
-    </div>
-  );
-}
+  const isScrolled = true;
 
-function NavLinks() {
   return (
-    <>
-      <li>
-        <DropdownLink href="/#portfolio">Portfolio</DropdownLink>
-      </li>
-      <li>
-        <DropdownLink href="/#about">About</DropdownLink>
-      </li>
-      <li>
-        <DropdownLink href="/resume" newTab>
-          Resume
-        </DropdownLink>
-      </li>
-      <li>
-        <DropdownLink href="/#about">Contact</DropdownLink>
-      </li>
-    </>
+    <nav
+      className={`fixed w-full top-0 z-50 flex justify-between items-center px-4 sm:px-6 lg:px-8 h-16 ${
+        isScrolled ? "bg-background/80 backdrop-blur-sm border-b shadow-sm" : "bg-transparent"
+      }`}
+    >
+      <Link href="/docs" legacyBehavior passHref>
+        MH
+      </Link>
+
+      <NavigationMenu className={`container mx-auto`}>
+        <NavigationMenuList>
+          <NavigationMenuItem>
+            <Link href="/docs" legacyBehavior passHref>
+              <NavigationMenuLink>About</NavigationMenuLink>
+            </Link>
+          </NavigationMenuItem>
+          <NavigationMenuItem>
+            <Link href="/docs" legacyBehavior passHref>
+              <NavigationMenuLink>Skills</NavigationMenuLink>
+            </Link>
+          </NavigationMenuItem>
+          <NavigationMenuItem>
+            <Link href="/docs" legacyBehavior passHref>
+              <NavigationMenuLink>Projects</NavigationMenuLink>
+            </Link>
+          </NavigationMenuItem>
+          <NavigationMenuItem>
+            <Link href="/docs" legacyBehavior passHref>
+              <NavigationMenuLink>Contact</NavigationMenuLink>
+            </Link>
+          </NavigationMenuItem>
+          <NavigationMenuItem>
+            <Link href="/docs" legacyBehavior passHref>
+              <NavigationMenuLink>Resume</NavigationMenuLink>
+            </Link>
+          </NavigationMenuItem>
+        </NavigationMenuList>
+      </NavigationMenu>
+
+      <Link href="/docs" legacyBehavior passHref>
+        🌞/🌜
+      </Link>
+    </nav>
   );
 }
