40eb6320c6cd591b287084536550ddeebd112bbf
Max Himmel <himmelmax@gmail.com>
2025-05-07 12:56:01 -0700

    fix layout and add theme toggle to navbar

--- src/app/(my-website)/navbar/navbar.tsx
+++ src/app/(my-website)/navbar/navbar.tsx
@@ -1,58 +1,63 @@
 import Link from "next/link";
-
 import {
   NavigationMenu,
   NavigationMenuItem,
   NavigationMenuLink,
   NavigationMenuList,
 } from "@/components/ui/navigation-menu";
+import { ThemeToggle } from "../theme/themeToggle";
 
 export function Navbar() {
   const isScrolled = true;
 
   return (
     <nav
-      className={`fixed w-full top-0 z-50 flex justify-between items-center px-4 sm:px-6 lg:px-8 h-16 ${
-        isScrolled ? "bg-background/80 backdrop-blur-sm border-b shadow-sm" : "bg-transparent"
+      className={`fixed w-full top-0 z-50 ${
+        isScrolled ? "bg-background/80 backdrop-blur-sm border-b shadow-xs" : "bg-transparent"
       }`}
     >
-      <Link href="/docs" legacyBehavior passHref>
-        MH
-      </Link>
+      <div className="container mx-auto px-4 sm:px-6 lg:px-8">
+        <div className="flex items-center justify-between h-16">
+          <a href="#home" className="text-2xl font-bold text-primary">
+            MH
+          </a>
 
-      <NavigationMenu className={`container mx-auto`}>
-        <NavigationMenuList>
-          <NavigationMenuItem>
-            <Link href="/docs" legacyBehavior passHref>
-              <NavigationMenuLink>About</NavigationMenuLink>
-            </Link>
-          </NavigationMenuItem>
-          <NavigationMenuItem>
-            <Link href="/docs" legacyBehavior passHref>
-              <NavigationMenuLink>Skills</NavigationMenuLink>
-            </Link>
-          </NavigationMenuItem>
-          <NavigationMenuItem>
-            <Link href="/docs" legacyBehavior passHref>
-              <NavigationMenuLink>Projects</NavigationMenuLink>
-            </Link>
-          </NavigationMenuItem>
-          <NavigationMenuItem>
-            <Link href="/docs" legacyBehavior passHref>
-              <NavigationMenuLink>Contact</NavigationMenuLink>
-            </Link>
-          </NavigationMenuItem>
-          <NavigationMenuItem>
-            <Link href="/docs" legacyBehavior passHref>
-              <NavigationMenuLink>Resume</NavigationMenuLink>
-            </Link>
-          </NavigationMenuItem>
-        </NavigationMenuList>
-      </NavigationMenu>
+          <NavigationMenu className={`container mx-auto`}>
+            <NavigationMenuList>
+              <NavigationMenuItem>
+                <Link href="/docs" legacyBehavior passHref>
+                  <NavigationMenuLink>About</NavigationMenuLink>
+                </Link>
+              </NavigationMenuItem>
+              <NavigationMenuItem>
+                <Link href="/docs" legacyBehavior passHref>
+                  <NavigationMenuLink>Skills</NavigationMenuLink>
+                </Link>
+              </NavigationMenuItem>
+              <NavigationMenuItem>
+                <Link href="/docs" legacyBehavior passHref>
+                  <NavigationMenuLink>Projects</NavigationMenuLink>
+                </Link>
+              </NavigationMenuItem>
+              <NavigationMenuItem>
+                <Link href="/docs" legacyBehavior passHref>
+                  <NavigationMenuLink>Contact</NavigationMenuLink>
+                </Link>
+              </NavigationMenuItem>
+              <NavigationMenuItem>
+                <Link href="/docs" legacyBehavior passHref>
+                  <NavigationMenuLink>Resume</NavigationMenuLink>
+                </Link>
+              </NavigationMenuItem>
+            </NavigationMenuList>
+          </NavigationMenu>
 
-      <Link href="/docs" legacyBehavior passHref>
+          <ThemeToggle />
+          {/* <Link href="/docs" legacyBehavior passHref>
         🌞/🌜
-      </Link>
+      </Link> */}
+        </div>
+      </div>
     </nav>
   );
 }
