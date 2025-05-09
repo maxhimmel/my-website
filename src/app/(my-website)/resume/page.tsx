4c654b94458a214a975c7326a42a0b04c1e1b921
Max Himmel <himmelmax@gmail.com>
2025-05-08 17:25:26 -0700

    get resume from new cms structure

--- src/app/(my-website)/resume/page.tsx
+++ src/app/(my-website)/resume/page.tsx
@@ -1,13 +1,17 @@
-import { getAboutMe } from "../about/profileService";
+import config from "@payload-config";
+import { getPayload } from "payload";
+import { Media } from "../../../../payload-types";
 
 export const revalidate = 86400;
 
 export default async function Resume() {
   // TODO: How do we handle error or loading?
 
-  const aboutMe = await getAboutMe();
+  const payload = await getPayload({ config });
+  const data = await payload.findGlobal({ slug: "resume" });
+  const resume = data.resume as Media;
 
-  return <iframe src={aboutMe.resume.url as string} title="Resume" className="w-full h-screen" />;
+  return <iframe src={resume.url!} title="Resume" className="w-full h-screen" />;
 }
 
 function ErrorFallback() {
