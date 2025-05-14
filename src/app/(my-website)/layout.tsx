d3f62c69c3a3745f207f80f852d1c6a79e8236cd
Max Himmel <himmelmax@gmail.com>
2025-05-14 16:04:19 -0700

    fix side-scrolling on mobile?!

--- src/app/(my-website)/layout.tsx
+++ src/app/(my-website)/layout.tsx
@@ -22,7 +22,7 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <body>
-        <div className="min-h-screen bg-background text-foreground">
+        <div className="min-h-screen bg-background text-foreground overflow-x-clip">
           <ThemeProvider defaultTheme="light" attribute="class">
             <Navbar />
             {children}
